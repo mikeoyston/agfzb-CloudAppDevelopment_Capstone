c78f57a9f2af7af73d79f92527ed384249561b0b
mikeoyston <oystonmichael@gmail.com>
2023-10-10 02:16:03 -0400

    week 4

--- functions/get-dealership.js
+++ functions/get-dealership.js
@@ -1,26 +1,36 @@
-/**
-  *
-  * main() will be run when you invoke this action
-  *
-  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *
-  * @return The output of this action, which must be a JSON object.
-  *
-  */
- const md5 = require('spark-md5');
+const { CloudantV1 } = require("@ibm-cloud/cloudant");
+const { IamAuthenticator } = require("ibm-cloud-sdk-core");
 
+function main(params) {
+  const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
+  const cloudant = CloudantV1.newInstance({
+    authenticator: authenticator,
+  });
+  cloudant.setServiceUrl(params.COUCH_URL);
+  console.log(params)
 
- function main(params) {
-   return {
-     entries: params.rows.map((row) => { return { 
-         id: row.doc.id,
-         city: row.doc.city,
-         state: row.doc.state,
-         st: row.doc.st,
-         address: row.doc.address,
-         zip: row.doc.zip,
-         lat: row.doc.lat,
-         long: row.doc.long,
-     }})
-   };
- }
+  if (params.state) {
+    selector = { state: params.state };
+  } else if (params.dealerId) {
+    selector = { id: parseInt(params.dealerId) };
+  } else {
+    selector = {};
+  }
+
+  let dealerListPromise = getMatchingRecords(cloudant, "dealerships", selector);
+  return dealerListPromise;
+}
+
+function getMatchingRecords(cloudant, dbname, selector) {
+  return new Promise((resolve, reject) => {
+    cloudant
+      .postFind({ db: dbname, selector: selector })
+      .then((result) => {
+        resolve({ result: result.result.docs });
+      })
+      .catch((err) => {
+        console.log(err);
+        reject({ err: err });
+      });
+  });
+}
